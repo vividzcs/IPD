f73a5f9245f848b06c11b3a18c91880f1642a8f6
outsider <787552171@qq.com>
2017-12-17 22:25:33 +0800

    Update CourseList.aspx.cs

--- Admin/Teacher/CourseList.aspx.cs
+++ Admin/Teacher/CourseList.aspx.cs
@@ -1,14 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Models;
 
 public partial class Admin_CourseList : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        /*var nowloginuser = Session["user"];
+        if (nowloginuser == null)
+        {
+            Response.Redirect("~/login.aspx");
+        }
+        else if (nowloginuser.GetType() != typeof(Teacher))
+        {
+            Response.Redirect("~/default.aspx");
+        }
+        else
+        {*/
+            //从数据库获取数据
+            //teachername = nowloginuser.Name;
+        //}
     }
 }
