56f63dc052ae96aec7aae921dd937c7feccadc12
HarborZeng <harborzeng@qq.com>
2017-12-18 17:41:53 +0800

    上传实验报告方式变更，新弹窗口
    上传实验报告方式变更，新弹窗口
    ！！！

---
+++ Class/UploadFile.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="UploadFile.aspx.cs" Inherits="Class_UploadFile" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <link href="../Content/form-controls.css" rel="stylesheet"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload runat="server" class="btn btn-info" ID="Uploader" accept="application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/msexcel,application/pdf,application/rtf,application/x-zip-compressed"/>
+            <asp:Button runat="server" Text="上传" CssClass="btn btn-primary" OnClick="FileUpload" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Class/UploadFile.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogicLayer.Impl;
+using Models;
+
+public partial class Class_UploadFile : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Request.QueryString["ceid"] == null)
+            Response.Redirect("Experiment.aspx");
+    }
+
+    protected void FileUpload(object sender, EventArgs e)
+    {
+        if (Uploader.HasFile)
+        {
+            if (Uploader.FileName.EndsWith(".doc", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".docx", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".ppt", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".pptx", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".7z", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".zip", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".pdf", true, CultureInfo.CurrentCulture) ||
+                Uploader.FileName.EndsWith(".zip", true, CultureInfo.CurrentCulture))
+            {
+                var savePath = Server.MapPath("~/UserUploads/Experiments/") +
+                               ((Student) Session["user"]).StudentNumber +
+                               "\\";
+                if (!Directory.Exists(savePath))
+                {
+                    Directory.CreateDirectory(savePath);
+                }
+                var millisecond = DateTime.Now.Millisecond;
+                Uploader.SaveAs(savePath + millisecond + Uploader.FileName);
+
+                var experiment = new Experiment()
+                {
+                    Name = millisecond + Uploader.FileName,
+                    Path = savePath
+                };
+
+                //由ceid获得courseExperiment
+                var ceid = 0;
+                if (Request.QueryString["ceid"] != null)
+                {
+                    ceid = int.Parse(Request.QueryString["ceid"]);
+                }
+
+                var succeed = new ExperimentServiceImpl().Submit(
+                    (CourseExperiment) (Session["ThatCourseExperiment" + ceid]),
+                    (Student) (Session["user"]), experiment);
+                Session.Remove("ThatCourseExperiment" + ceid);
+                Response.Write(succeed == 1
+                    ? "<script>alert(\"上传成功！\");</script>"
+                    : "<script>alert(\"上传失败请重试！\");</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert(\"不能上传的格式！\");</script>");
+            }
+        }
+        else
+        {
+            Response.Write("<script>alert(\"请先选择文件再点击上传\");</script>");
+        }
+    }
+}
