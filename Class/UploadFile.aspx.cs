8652ff9a2056de4ca43e59fc20cdc37cbc5dae22
HarborZeng <harborzeng@qq.com>
2017-12-20 13:49:14 +0800

    上传实验报告，作业统一化

--- Class/UploadFile.aspx.cs
+++ Class/UploadFile.aspx.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using BusinessLogicLayer.Impl;
 using Models;
 
@@ -13,22 +8,30 @@ public partial class Class_UploadFile : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Request.QueryString["ceid"] == null)
-            Response.Redirect("Experiment.aspx");
+        
     }
 
     protected void FileUpload(object sender, EventArgs e)
     {
-        if (Uploader.HasFile)
+        if (!Uploader.HasFile)
         {
-            if (Uploader.FileName.EndsWith(".doc", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".docx", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".ppt", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".pptx", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".7z", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".zip", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".pdf", true, CultureInfo.CurrentCulture) ||
-                Uploader.FileName.EndsWith(".zip", true, CultureInfo.CurrentCulture))
+            Response.Write("<script>alert(\"请先选择文件再点击上传\");</script>");
+        }
+        else
+        {
+            if (!(Uploader.FileName.EndsWith(".doc", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".docx", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".ppt", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".pptx", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".7z", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".tar.gz", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".pdf", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".cvs", true, CultureInfo.CurrentCulture) ||
+                  Uploader.FileName.EndsWith(".zip", true, CultureInfo.CurrentCulture)))
+            {
+                Response.Write("<script>alert(\"不能上传的格式！\");</script>");
+            }
+            else
             {
                 var savePath = Server.MapPath("~/UserUploads/Experiments/") +
                                ((Student) Session["user"]).StudentNumber +
@@ -40,35 +43,50 @@ public partial class Class_UploadFile : System.Web.UI.Page
                 var millisecond = DateTime.Now.Millisecond;
                 Uploader.SaveAs(savePath + millisecond + Uploader.FileName);
 
-                var experiment = new Experiment()
-                {
-                    Name = millisecond + Uploader.FileName,
-                    Path = savePath
-                };
-
-                //由ceid获得courseExperiment
-                var ceid = 0;
+                var succeed = 0;
                 if (Request.QueryString["ceid"] != null)
                 {
-                    ceid = int.Parse(Request.QueryString["ceid"]);
+                    var experiment = new Experiment()
+                    {
+                        Name = millisecond + Uploader.FileName,
+                        Path = savePath.Remove(0, Server.MapPath("~").Length - 1)
+                    };
+
+                    //由ceid获得courseExperiment
+
+                    var ceid = int.Parse(Request.QueryString["ceid"]);
+
+                    succeed = new ExperimentServiceImpl().Submit(
+                        (CourseExperiment) (Session["ThatCourseExperiment" + ceid]),
+                        (Student) (Session["user"]), experiment);
+                    Session.Remove("ThatCourseExperiment" + ceid);
+                }
+                else if (Request.QueryString["chid"] != null)
+                {
+                    var homework = new Homework()
+                    {
+                        Name = millisecond + Uploader.FileName,
+                        Path = savePath.Remove(0, Server.MapPath("~").Length - 1)
+                    };
+
+                    //由ceid获得courseExperiment
+
+                    var chid = int.Parse(Request.QueryString["chid"]);
+
+                    succeed = new HomeworkServiceImpl().Submit(
+                        (CourseHomework) (Session["ThatCourseHomework" + chid]),
+                        (Student) (Session["user"]), homework);
+                    Session.Remove("ThatCourseExperiment" + chid);
+                }
+                else
+                {
+                    Response.Write("<script>出现异常</script>");
                 }
 
-                var succeed = new ExperimentServiceImpl().Submit(
-                    (CourseExperiment) (Session["ThatCourseExperiment" + ceid]),
-                    (Student) (Session["user"]), experiment);
-                Session.Remove("ThatCourseExperiment" + ceid);
                 Response.Write(succeed == 1
-                    ? "<script>alert(\"上传成功！\");</script>"
+                    ? "<script>alert(\"上传成功！\");window.close();opener.location.reload();</script>"
                     : "<script>alert(\"上传失败请重试！\");</script>");
             }
-            else
-            {
-                Response.Write("<script>alert(\"不能上传的格式！\");</script>");
-            }
-        }
-        else
-        {
-            Response.Write("<script>alert(\"请先选择文件再点击上传\");</script>");
         }
     }
 }
