9bde3e43518811b8cd2cc1ee8b22c8dcb7a7dda5
HarborZeng <harborzeng@qq.com>
2017-12-23 00:19:13 +0800

    找了个库来完成打包下载

--- Admin/Teacher/BatchDownload.aspx.cs
+++ Admin/Teacher/BatchDownload.aspx.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using BusinessLogicLayer.Impl;
+using ICSharpCode.SharpZipLib.Zip;
 
 public partial class Admin_Teacher_BatchDownload : System.Web.UI.Page
 {
@@ -29,6 +30,22 @@ public partial class Admin_Teacher_BatchDownload : System.Web.UI.Page
                 (current, homework) => current + ("http://" + Request.Url.Host + homework.Path + Environment.NewLine));
             downloadUrLs = downloadUrLs.Replace("\\", "/");
             Response.Write(downloadUrLs);
+
+            var path = homeworks.Select(homework => homework.Path).ToList();
+
+
+            var buffer = Download(path.ToArray());
+            if (buffer != null)
+            {
+                Response.Clear();
+                Response.Charset = "GB2312";
+                Response.ContentEncoding = System.Text.Encoding.UTF8;
+                Response.AddHeader("Content-Disposition",
+                    "attachment; filename=" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+                Response.BinaryWrite(buffer); //输出
+                Response.Flush();
+                Response.End();
+            }
 
             /*var command = "7z a homework.zip ";
             foreach (var homework in homeworks)
@@ -59,16 +76,83 @@ public partial class Admin_Teacher_BatchDownload : System.Web.UI.Page
 
             Response.Write(File.Exists("~/homework.zip"));
             Response.Write(output);*/
-        }else if (pageType == "ex")
+        }
+        else if (pageType == "ex")
         {
             var service = new ExperimentServiceImpl();
             var experiments = service.GetExpermentByCourseExperimentId(id);
 
             var downloadUrLs = experiments.Aggregate("",
-                (current, experiment) => current + ("http://" + Request.Url.Host + experiment.Path + Environment.NewLine));
+                (current, experiment) =>
+                    current + ("http://" + Request.Url.Host + experiment.Path + Environment.NewLine));
             downloadUrLs = downloadUrLs.Replace("\\", "/");
             Response.Write(downloadUrLs);
 
+            var path = experiments.Select(homework => homework.Path).ToList();
+
+
+            var buffer = Download(path.ToArray());
+            if (buffer != null)
+            {
+                Response.Clear();
+                Response.Charset = "GB2312";
+                Response.ContentEncoding = System.Text.Encoding.UTF8;
+                Response.AddHeader("Content-Disposition",
+                    "attachment; filename=" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".zip");
+                Response.BinaryWrite(buffer); //输出
+                Response.Flush();
+                Response.End();
+            }
         }
+    }
+
+    ///批量下载
+    ///参数string[]数组，需要传物理路径
+    ///返回值byte[]数组
+    public byte[] Download(string[] path)
+    {
+        byte[] buffer = null;
+        using (MemoryStream ms = new MemoryStream())
+        {
+            using (ZipFile zip = ZipFile.Create(ms))
+            {
+                zip.BeginUpdate();
+                zip.NameTransform = new MyNameTranform(); //使用自定义MyNameTranform类，改变打包路径
+
+                if (path.Length == 0)
+                {
+                    Response.Write("<script>alert(\"没有人交了这个作业\");</script>");
+                    return null;
+                }
+
+                //遍历path放入zip中
+                foreach (var p in path)
+                {
+                    zip.Add(Server.MapPath("~") + p);
+                }
+                zip.CommitUpdate();
+
+                buffer = new byte[ms.Length];
+                ms.Position = 0;
+                ms.Read(buffer, 0, buffer.Length); //读入buffer
+            }
+        }
+
+        return buffer;
+    }
+}
+
+///修改打包路径，仅需要文件名和扩展名即可
+public class MyNameTranform : ICSharpCode.SharpZipLib.Core.INameTransform
+{
+    public string TransformDirectory(string name)
+    {
+        return null;
+    }
+
+    ///返回文件名和扩展名
+    public string TransformFile(string name)
+    {
+        return Path.GetFileName(name);
     }
 }
