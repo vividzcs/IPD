20c242330a838b276097cede6bfb20e987e8bd1c
HarborZeng <harborzeng@qq.com>
2017-12-23 23:23:30 +0800

    添加用户级别检测方法

--- App_Code/Utils/AuthHelper.cs
+++ App_Code/Utils/AuthHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.SessionState;
+using Models;
 
 namespace Utils
 {
@@ -13,6 +14,35 @@ namespace Utils
             if (session["user"] == null)
                 response.Redirect("~/Login.aspx?pre=" + server.UrlEncode(request.Url.AbsoluteUri));
 
+        }
+
+
+        public static void TeacherOnlyPage(HttpSessionState session, HttpRequest request, HttpResponse response,
+            HttpServerUtility server)
+        {
+            if (!(session["user"] is Teacher))
+            {
+                session.RemoveAll();
+                response.Redirect("/Login.aspx");
+            }
+        }
+        public static void AdminOnlyPage(HttpSessionState session, HttpRequest request, HttpResponse response,
+            HttpServerUtility server)
+        {
+            if (!(session["user"] is Admin))
+            {
+                session.RemoveAll();
+                response.Redirect("/Login.aspx");
+            }
+        }
+        public static void StudentOnlyPage(HttpSessionState session, HttpRequest request, HttpResponse response,
+            HttpServerUtility server)
+        {
+            if (!(session["user"] is Student))
+            {
+                session.RemoveAll();
+                response.Redirect("/Login.aspx");
+            }
         }
     }
 }
