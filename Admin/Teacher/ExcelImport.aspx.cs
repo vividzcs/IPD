a17ef77ad794827d813aadde3cbc55319a1acf25
HarborZeng <harborzeng@qq.com>
2017-12-23 14:37:31 +0800

    完成excel导入学生名单
    名单格式详见TeacherUpload\StudentExcelList\下的例子，注：成功导入需要您的电脑上安装有AccessDatabaseEngine.exe引擎，下载地址：http://download.microsoft.com/download/7/0/3/703ffbcb-dc0c-4e19-b0da-1463960fdcdb/AccessDatabaseEngine.exe

--- Admin/Teacher/ExcelImport.aspx.cs
+++ Admin/Teacher/ExcelImport.aspx.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Models;
 using Utils;
 
 public partial class Admin_Teacher_ExcelImport : System.Web.UI.Page
@@ -14,19 +15,27 @@ public partial class Admin_Teacher_ExcelImport : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         AuthHelper.AuthCheck(Session, Request, Response, Server);
+
+        if (Request.QueryString["course"] == null)
+        {
+            Response.Redirect("CourseList.aspx");
+        }
     }
 
     protected void ExcelUpload(object sender, EventArgs e)
     {
+        //确保上传的有文件且文件是excel格式！
         if ((!Uploader.HasFile || Uploader.PostedFile.ContentType != "application/vnd.ms-excel") &&
             Uploader.PostedFile.ContentType !=
             "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") return;
         try
         {
+            //把文件放到TeacherUpload/StudentExcelList目录下
             string fileName = Path.Combine(Server.MapPath("~/TeacherUpload/StudentExcelList"),
                 Guid.NewGuid() + Path.GetExtension(Uploader.PostedFile.FileName));
             Uploader.PostedFile.SaveAs(fileName);
 
+            //创建excel访问的数据源
             string conString = "";
             string ext = Path.GetExtension(Uploader.PostedFile.FileName);
             if (ext.ToLower() == ".xls")
@@ -40,8 +49,9 @@ public partial class Admin_Teacher_ExcelImport : System.Web.UI.Page
                             ";Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=2\"";
             }
 
+            //按照数据源使用SQL语句查询excel条目
             string query =
-                "Select [姓名],[学号] from [StudentData$]";
+                "Select [姓名],[学号] from [Sheet1$]";
             OleDbConnection con = new OleDbConnection(conString);
             if (con.State == ConnectionState.Closed)
             {
@@ -56,45 +66,35 @@ public partial class Admin_Teacher_ExcelImport : System.Web.UI.Page
             con.Close();
             con.Dispose();
 
-            Response.Write(ds.Tables[0].Rows[0]);
-
-            // Import to Database
-            /*using (MuDatabaseEntities dc = new MuDatabaseEntities())
-            {
-                foreach (DataRow dr in ds.Tables[0].Rows)
+            //利用LINQ语句把查出来的学生放在sList列表中
+            var sList = (from DataRow dr in ds.Tables[0].Rows
+                let name = dr["姓名"].ToString()
+                let studentNumber = dr["学号"].ToString()
+                select new Student()
                 {
-                    string empID = dr["Employee ID"].ToString();
-                    var v = dc.EmployeeMasters.Where(a => a.EmployeeID.Equals(empID)).FirstOrDefault();
-                    if (v != null)
-                    {
-                        // Update here
-                        v.CompanyName = dr["Company Name"].ToString();
-                        v.ContactName = dr["Contact Name"].ToString();
-                        v.ContactTitle = dr["Contact Title"].ToString();
-                        v.EmployeeAddress = dr["Employee Address"].ToString();
-                        v.PostalCode = dr["Postal Code"].ToString();
-                    }
-                    else
-                    {
-                        // Insert
-                        dc.EmployeeMasters.Add(new EmployeeMaster
-                        {
-                            EmployeeID = dr["Employee ID"].ToString(),
-                            CompanyName = dr["Company Name"].ToString(),
-                            ContactName = dr["Contact Name"].ToString(),
-                            ContactTitle = dr["Contact Title"].ToString(),
-                            EmployeeAddress = dr["Employee Address"].ToString(),
-                            PostalCode = dr["Postal Code"].ToString()
-                        });
-                    }
-                }
+                    Name = name,
+                    StudentNumber = studentNumber
+                }).ToList();
 
-                dc.SaveChanges();
-            }*/
+            //拿到URL里面传过来的courseid
+            var courseId = int.Parse(Request.QueryString["course"]);
+
+            //通过courseId和学生id创建学生的选课关系CourseSelection对象，并把CourseSelection返回值放在courseSelections变量里面，下面用
+            //学生id通过学号拿到
+            var courseSelections = sList.Select(student => new StudentServiceImpl().GetByStudentNumber(student.StudentNumber))
+                .Select(s => new CourseSelectionServiceImpl().CreateCourseSelectRecording(new CourseSelection()
+                {
+                    StudentId = s.StudentId,
+                    CourseId = courseId
+                }))
+                .ToList();
+
+            //输出添加成功的条目数
+            Response.Write("<script>alert(\"成功添加" + courseSelections.Count + "名学生到该课程！\");window.close();</script>");
         }
-        catch (Exception)
+        catch (Exception ex)
         {
-            throw;
+            Response.Write("<script>alert(\"添加失败，失败信息: "+ex.Message+"\");window.close();</script>");
         }
     }
 }
