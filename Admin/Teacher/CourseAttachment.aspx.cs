01f2a230b72fd999b2b0b4a4cfadebcda061fde1
Amoswish <1572912653@qq.com>
2017-12-29 22:37:31 +0800

    无法解决删不了具体文件的问题，其余都好了

--- Admin/Teacher/CourseAttachment.aspx.cs
+++ Admin/Teacher/CourseAttachment.aspx.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using BusinessLogicLayer.Impl;
 using Models;
 using Utils;
-
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
 namespace Admin.Teacher
 {
     public partial class Admin_CourseAttachment : System.Web.UI.Page
@@ -37,7 +39,7 @@ namespace Admin.Teacher
             }
 
 
-            var courseAttachment = new AttachmentServiceImpl();
+            var courseAttachment = new CourseAttachmentServiceimpl();
             var teacherCourse = new CourseServiceImpl();
             //获得当前老师教的课程
             var nowCourse = new CourseServiceImpl().GetByCourseId(int.Parse(cidString));
@@ -46,18 +48,48 @@ namespace Admin.Teacher
             //var nowAttachment_id =((CourseAttachment)thisTeacherAttachment).AttachmentId;
             try
             {
-                var neededDeleteAttachmentId = int.Parse(Request.Form.Keys[1]);
-                if (new AttachmentServiceImpl().DeleteByAttachemtId(neededDeleteAttachmentId))
+                //通过js修改表单中提交的值，然后遍历正则找到 xxneededdeleted
+                string pattern = @"\d{1,}neededdeleted";
+                int neededDeleteAttachmentId;
+                for (int i = 0; i < Request.Form.Count; i++)
                 {
-                    //删除成功
-                    Response.Write("<script language='javascript'>alert('删除成功！');</script>");
-                    Response.Redirect(Request.Url.ToString());
+                    if (Regex.IsMatch(Request.Form.Keys[i], pattern))
+                    {
+                        neededDeleteAttachmentId = int.Parse(Request.Form.Keys[i].ToString().Split('n')[0]);
+                        if (new CourseAttachmentServiceimpl().DeleteByAttachemtId(neededDeleteAttachmentId))
+                        {
+                            //删除数据库成功
+                            //删除当前目录下文件
+                            var deletePath = new CourseAttachmentServiceimpl().GetByAttachmentId(neededDeleteAttachmentId).path;
+                            string temp = deletePath.Path;
+                            string a = "~" + temp;
+                            FileInfo file = new FileInfo(Server.MapPath(a));//指定文件路径
+                            var b = "ss";
+                            try//判断文件是否存在
+                            {
+                                file.Attributes = FileAttributes.Normal;//将文件属性设置为普通,比方说只读文件设置为普通
+                                file.Delete();//删除文件
+                                //Response.Write("<script language='javascript'>alert('删除成功！请按F5刷新');</script>");
+                                Response.Redirect(Request.Url.ToString());
+                            }
+                            catch (Exception ex)
+                            { }
+                            finally
+                            {
+                                Response.Redirect(Request.Url.ToString());
+                            }
+                            Response.Redirect(Request.Url.ToString());
+
+                        }
+                        else
+                        {
+                            //删除失败
+                            Response.Write("<script language='javascript'>alert('删除失败！请重试');window.location.reload();</script>");
+                        }
+                    }
                 }
-                else
-                {
-                    //删除失败
-                    Response.Write("<script language='javascript'>alert('删除失败！');</script>");
-                }
+                
+
             }
             catch (Exception)
             {
@@ -66,23 +98,43 @@ namespace Admin.Teacher
             }
 
         }
-
-
-
-
-
+        //上传课件代码
         protected void UploadCourseAttachment_Command(object sender, System.Web.UI.WebControls.CommandEventArgs e)
         {
-            String savePath = @"";//此处savePath未加入，应该填入相应路径
-            if (FileUploadCourseAttachment.HasFile)
+            string savePath = @"/TeacherUpload/CourseWare/";//此处savePath未加入，应该填入相应路径
+            string teacherJobNum = "";
+            if (Session["user"] is Models.Teacher t)
             {
-                String filename;
-                filename = FileUploadCourseAttachment.FileName;
-                savePath += filename;
-                FileUploadCourseAttachment.SaveAs(savePath);
-                Response.Write("<script language='javascript'>alert('上传成功！');</script>");
-                Response.Redirect(Request.Url.ToString());
+                teacherJobNum = t.JobNumber;
+                if (FileUploadCourseAttachment.HasFile)
+                {
 
+                    //上传到服务器文件夹中
+                    String filename;
+                    filename = FileUploadCourseAttachment.FileName;
+                    //存储路径为当前路径+老师工号
+                    string webSavePath = Server.MapPath("~" + savePath + "/" + teacherJobNum + "/" + filename);
+                    FileUploadCourseAttachment.SaveAs(webSavePath);
+
+                    //插入到数据库中
+                    var neededInsertCourseAttachment = new CourseAttachment();
+                    neededInsertCourseAttachment.Name = filename;
+                    //通过session获得课程id
+                    string cidString = Request.QueryString["course"];
+                    neededInsertCourseAttachment.CourseId = int.Parse(cidString);
+                    neededInsertCourseAttachment.Path = savePath + teacherJobNum + filename;
+                    neededInsertCourseAttachment.IssuedTime = DateTime.Now.ToLocalTime();
+                    if (new CourseAttachmentServiceimpl().Create(neededInsertCourseAttachment) > 0)
+                    {
+                        
+                        Response.Redirect(Request.Url.ToString());
+                    }
+                    else
+                    {
+                        Response.Write("<script language='javascript'>alert('上传失败，请重试！');</script>");
+                    }
+
+                }
             }
             else
             {
--- App_Code/DataAccessLayer/Interface/ICourseAttachmentDal.cs
+++ App_Code/DataAccessLayer/Interface/ICourseAttachmentDal.cs
@@ -12,5 +12,34 @@ namespace BusinessLogicLayer.Impl
     public interface ICourseAttachmentDal
     {
         int InsertCourseAttachment(CourseAttachment courseAttachment);
+        /// <summary>
+        /// IAttachmentDal 的摘要说明
+        /// </summary>
+        /// <summary>
+        /// 通过课程id查找课程课件
+        /// </summary>
+        /// <param name="course">封装了课程id的课程对象</param>
+        /// <returns>课程课件的枚举器</returns>
+        IEnumerable<CourseAttachment> SelectByCourse(Course course);
+
+        /// <summary>
+        /// ICourseAttachmentDal 的摘要说明
+        /// </summary>
+        /// <summary>
+        /// 通过课件Id删除课件
+        /// </summary>
+        /// <param name="neededDeleteAttachmentId">需要删除的课件id</param>
+        /// <returns>是否删除成功的bool类型</returns>
+        bool DeleteByAttachemtId(int neededDeleteAttachmentId);
+
+        /// <summary>
+        /// ICourseAttachmentDal 的摘要说明
+        /// </summary>
+        /// <summary>
+        /// 通过课件Id找到课件
+        /// </summary>
+        /// <param name="attachmentId">需要查找的课件id</param>
+        /// <returns>一个课件/returns>
+        CourseAttachment SelectByAttachmentId(int attachmentId);
     }
 }
